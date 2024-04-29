1c82bce09ecd06d353340ff1ed79cdea260ee2a4
Patrick Periyanayagam I <111488879+Patrickperiyanayagam@users.noreply.github.com>
2024-04-29 15:37:06 +0530

    Delete Day14 directory

--- Day14/cw2/src/main/java/com/example/cw2/repository/BookRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.cw2.repository;
-import com.example.cw2.model.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepo extends JpaRepository<Book, Long> {
-}
-
